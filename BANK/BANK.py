1fdb3858dd28f84d8fe9f1602e163efbe4aa6bbd
Tanish74-51 <tanish.s7451@gmail.com>
2026-01-01 23:07:57 +0530

    system launched

--- BANK/BANK.py
+++ BANK/BANK.py
@@ -1,4 +1,21 @@
+import json
 
+try:
+    with open('bank_data','r') as f:
+        bank_data=json.load(f)
+except (FileNotFoundError,json.JSONDecodeError):
+    bank_data=[]
+
+
+
+
+
+
+
+
+
+with open('bank_data','w') as f:
+    json.dump(bank_data,f,indent=4)
 
 
 
